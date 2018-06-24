bedf6e23070f1b0334867a3262cc6db97c4620aa
Wladek <wladek.airo@gmail.com>
2018-06-24 03:01:58 +0300

    Small change

--- src/java/orders/changeProductStatus.java
+++ src/java/orders/changeProductStatus.java
@@ -60,11 +60,11 @@ public class changeProductStatus extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doPost(request, response);
-    }
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        doPost(request, response);
+//    }
 
     /**
      * Handles the HTTP <code>POST</code> method.
